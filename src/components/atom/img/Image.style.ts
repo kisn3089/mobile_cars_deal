5411d78b418c2e79150bcadcc4d77d6a65ed7ae5
Stemps <kisn3089@naver.com>
2024-08-10 15:50:30 +0900

    [UI]Img bgColor 삭제

--- src/components/atom/img/Image.style.ts
+++ src/components/atom/img/Image.style.ts
@@ -2,10 +2,6 @@ import styled from "styled-components";
 import { ImageType } from "./Image.type";
 
 export const StyleImg = styled.img<ImageType>`
-  ${({ theme }) => `
-        object-fit: contain;
-        border-radius: 10px;
-        background-color: ${theme.palette.gray.gray200};
-        padding: 8px;
-    `}
+  object-fit: contain;
+  border-radius: 10px;
 `;
