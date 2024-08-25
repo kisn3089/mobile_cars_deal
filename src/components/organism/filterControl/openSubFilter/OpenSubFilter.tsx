5dff4abcb9638e690e5a24e4523052d6b2cbdccf
Stemps <kisn3089@naver.com>
2024-08-25 11:41:07 +0900

    [Refectoring]하위 필터링 아이템 데이터 타입에 따라 자동화

--- src/components/organism/filterControl/openSubFilter/OpenSubFilter.tsx
+++ src/components/organism/filterControl/openSubFilter/OpenSubFilter.tsx
@@ -3,14 +3,25 @@ import TagsFilter from "../tagsFilter/TagsFilter";
 import SortFilter from "../sortFilter/SortFilter";
 import { GetCarListSuspense } from "@/api/GetCarList";
 
+type PickFilterItem = { price: string[]; tags: string[] };
+
 type OpenSubFilterProps = {
   isOpenFilter: string | null;
   onSetFilter: (e: React.MouseEvent<HTMLButtonElement>, key: string) => void;
 };
 
 const OpenSubFilter = ({ isOpenFilter, onSetFilter }: OpenSubFilterProps) => {
-  const { data: getListCar, isSuccess } = GetCarListSuspense();
-  if (isSuccess) console.log(getListCar);
+  const { data: getListCar } = GetCarListSuspense();
+
+  // const pickFilterItem = getListCar.reduce((acc, cur): PickFilterItem => {
+  //   const originPrice = Math.floor(parseInt(cur.discount()));
+  //   const toUnit =
+  //     originPrice.toString().length === 1
+  //       ? `${originPrice}000`
+  //       : `${originPrice.toString().slice(0, 1)}00`;
+  //   console.log(toUnit);
+  //   if (acc.price.includes(toUnit)) return acc;
+  // }, {} as PickFilterItem);
 
   if (isOpenFilter === "price")
     return <PriceFilter onPriceFilter={(e) => onSetFilter(e, "price")} />;
