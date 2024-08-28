2a0720480175c7a83a3d5095f9c6117cb001853d
Stemps <kisn3089@naver.com>
2024-08-28 23:19:09 +0900

    [Refectoring]CardInfoCar 폴더 소문자로 수정 & 필터 가격태그 리스트 데이터값에 따라 생성되게 자동화

--- src/components/organism/filterControl/priceFilter/PriceFilter.tsx
+++ src/components/organism/filterControl/priceFilter/PriceFilter.tsx
@@ -1,6 +1,9 @@
 import { useSearchParams } from "react-router-dom";
 import { ItemFilter } from "../FilterControl.style";
 import { OptionListLayout } from "./PriceFilter.style";
+import { GetCarListSuspense } from "@/api/GetCarList";
+
+type PickPriceRangeType = { label: string; value: number };
 
 type PriceFilterProps = {
   onPriceFilter: (e: React.MouseEvent<HTMLButtonElement>) => void;
@@ -16,13 +19,29 @@ const priceOptions = [
 const PriceFilter = ({ onPriceFilter }: PriceFilterProps) => {
   const [query] = useSearchParams();
   const parsingPrice = query.get("price");
+  const { data: getListCar } = GetCarListSuspense();
+  // console.log(getListCar);
+
+  const pickPriceRange = getListCar
+    .reduce<PickPriceRangeType[]>((acc, cur) => {
+      const originPrice = Math.floor(parseInt(cur.discount()));
+      const toUnit =
+        originPrice.toString().length === 1
+          ? `${originPrice}000`
+          : `${originPrice.toString().slice(0, 1)}00`;
+      if (!acc.find((origin) => origin.value === +toUnit))
+        return [...acc, { label: `${+toUnit / 10}만 이상`, value: +toUnit }];
+
+      return acc;
+    }, [] as PickPriceRangeType[])
+    .sort((a, b) => a.value - b.value);
 
   return (
     <OptionListLayout>
-      {priceOptions.map((priceOption, i) => (
+      {pickPriceRange.map((priceOption, i) => (
         <ItemFilter
           key={i}
-          $isActive={parsingPrice === priceOption.value}
+          $isActive={parsingPrice === priceOption.value.toString()}
           onClick={onPriceFilter}
           value={priceOption.value}>
           {priceOption.label}
