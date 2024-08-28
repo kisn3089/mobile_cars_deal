4cc6a24efc7a8cac421f6c18a11ba55aec782d4c
Stemps <kisn3089@naver.com>
2024-08-28 23:35:08 +0900

    [Fix]Sort 기능 100만원 넘어가면 가장 작은 수로 계산하여 sort 기능 제대로 안되는 오류 수정

--- src/components/organism/filterControl/priceFilter/PriceFilter.tsx
+++ src/components/organism/filterControl/priceFilter/PriceFilter.tsx
@@ -9,22 +9,15 @@ type PriceFilterProps = {
   onPriceFilter: (e: React.MouseEvent<HTMLButtonElement>) => void;
 };
 
-const priceOptions = [
-  { label: "10만 이상", value: "100" },
-  { label: "20만 이상", value: "200" },
-  { label: "30만 이상", value: "300" },
-  { label: "40만 이상", value: "400" },
-];
-
 const PriceFilter = ({ onPriceFilter }: PriceFilterProps) => {
   const [query] = useSearchParams();
   const parsingPrice = query.get("price");
   const { data: getListCar } = GetCarListSuspense();
-  // console.log(getListCar);
 
   const pickPriceRange = getListCar
     .reduce<PickPriceRangeType[]>((acc, cur) => {
       const originPrice = Math.floor(parseInt(cur.discount()));
+
       const toUnit =
         originPrice.toString().length === 1
           ? `${originPrice}000`
--- src/util/filteredListCar.ts
+++ src/util/filteredListCar.ts
@@ -18,7 +18,9 @@ export const filteredBySearch = ({
 
   const { search, price, sort, tags } = filterKey;
   const filter = getListCar.reduce<CarInfoWithPrice[]>((acc, cur) => {
-    const filterPrice = price ? parseInt(cur.discount()) >= +price : true;
+    const filterPrice = price
+      ? toNumber(cur.discount()) / 1000 >= +price
+      : true;
     const filterSearch = search ? cur.carClassName.includes(search) : true;
     const filterTags = tags ? cur.carTypeTags.includes(tags) : true;
 
@@ -32,8 +34,8 @@ export const filteredBySearch = ({
   if (sort && filter)
     return filter.sort((a, b) =>
       sort === "up"
-        ? parseInt(b.discount()) - parseInt(a.discount())
-        : parseInt(a.discount()) - parseInt(b.discount())
+        ? toNumber(b.discount()) - toNumber(a.discount())
+        : toNumber(a.discount()) - toNumber(b.discount())
     );
   return filter;
 };
@@ -43,3 +45,5 @@ export const specialOnly = (getListCar: CarInfoWithPrice[]) =>
     if (cur.carTypeTags.includes("특가")) return [...acc, cur];
     return acc;
   }, []);
+
+const toNumber = (price: string) => parseInt(price.replace(/,/g, ""));
