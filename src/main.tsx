5bc41769784b21340db4ae5d110ddbf877c35f8c
Stemps <kisn3089@naver.com>
2024-08-29 23:45:35 +0900

    [Build]하기 위해 변경점

--- src/main.tsx
+++ src/main.tsx
@@ -13,14 +13,6 @@ const queryClient = new QueryClient({
   },
 });
 
-// (done) 1. search 기능 ( 차량 이름으로 검색 &carClassName )
-// (done) 2. filter 기능
-//    - 차량 가액으로 4가지 정도의 넛지를 줌 &price
-//    - tag에 포함되는 차량 filter &carTypeTags
-// (done) 3. sort 기능 ( 년식 year, 키로수 &drivingDistance )
-// 4. 최초 1회 애니메이션 ( 단축키 설명 및 감사 인사 ) ( LocalStorage 활용 )
-// (done) 5.Error 핸들링과 Loading 상태 보여줄 수 있는 설정 구현
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <RootLayout>
     <QueryClientProvider client={queryClient}>
